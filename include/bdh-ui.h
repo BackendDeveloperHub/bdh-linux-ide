adfb86c16af3859bf95c526133229574dfa8d48b
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-26 22:22:14 +0530

    Create bdh-ui.h for UI and state variable declarations
    Add header file for UI components and state variables

---
+++ include/bdh-ui.h
@@ -0,0 +1,27 @@
+#ifndef BDH_UI_H
+#define BDH_UI_H
+
+#include "bdh-db.h"
+
+// டைனமிக் UI வேரியபிள்கள் (bdh-ui.c -ல் வரையறுக்கப்படும்)
+extern int term_height;
+extern float tree_width_ratio;
+
+// Main State வேரியபிள்கள் (main.c -ல் இருந்து பகிரப்படுபவை)
+extern int focus; 
+extern int total_rows;
+extern int total_cols;
+extern int divider_col;
+
+extern char db_query_buf[1024];
+extern int db_query_len;
+extern PGconn *db_conn;
+
+extern char term_cmd_buf[1024];
+extern int term_cmd_len;
+extern char term_output[7][1024];
+
+// UI ஃபங்ஷன்
+void draw_ui();
+
+#endif
