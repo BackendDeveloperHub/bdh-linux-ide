d6f15927dc5018bef526e48d14b6ade11a0e8874
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-27 21:50:27 +0530

    Implement cursor tracking in editor
    Added cursor tracking variables and updated cursor position logic in the editor.

--- src/bdh-edit.c
+++ src/bdh-edit.c
@@ -7,6 +7,24 @@ char editor_buf[EDITOR_BUF_SIZE] = "";
 int editor_len = 0;
 char current_file[256] = "untitled.txt";
 
+// 🔥 UI Cursor Tracking Variables
+int editor_cx = 0;
+int editor_cy = 0;
+
+// கர்சர் இடத்தை அப்டேட் செய்யும் ஃபங்ஷன்
+void update_cursor_position() {
+    editor_cx = 0;
+    editor_cy = 0;
+    for (int i = 0; i < editor_len; i++) {
+        if (editor_buf[i] == '\n') {
+            editor_cy++;
+            editor_cx = 0;
+        } else {
+            editor_cx++;
+        }
+    }
+}
+
 // 1. ஃபைலை டிஸ்க்கில் இருந்து பஃபருக்கு ஏற்றும் லாஜிக்
 void load_file_to_editor(const char *filename) {
     FILE *f = fopen(filename, "r");
@@ -14,12 +32,14 @@ void load_file_to_editor(const char *filename) {
         snprintf(editor_buf, EDITOR_BUF_SIZE, "// New File: %s\n", filename);
         editor_len = strlen(editor_buf);
         strncpy(current_file, filename, sizeof(current_file) - 1);
+        update_cursor_position();
         return;
     }
     editor_len = fread(editor_buf, 1, EDITOR_BUF_SIZE - 1, f);
     editor_buf[editor_len] = '\0';
     fclose(f);
     strncpy(current_file, filename, sizeof(current_file) - 1);
+    update_cursor_position();
 }
 
 // 2. பஃபரில் உள்ளதை டிஸ்க்கில் சேவ் செய்யும் லாஜிக்
@@ -43,18 +63,22 @@ int handle_editor_input(char c) {
         if (editor_len < EDITOR_BUF_SIZE - 1) {
             editor_buf[editor_len++] = c;
             editor_buf[editor_len] = '\0';
+            editor_cx++; // வலது பக்கம் கர்சரை நகர்த்துகிறோம்
         }
     }
     else if (c == 127 || c == 8) { // Backspace
         if (editor_len > 0) {
             editor_len--;
             editor_buf[editor_len] = '\0';
+            update_cursor_position(); // பேக்ஸ்பேஸ் அடித்தால் கர்சரை ரீ-கால்குலேட் செய்கிறோம்
         }
     }
     else if (c == '\n' || c == '\r') { // Enter பட்டன்
         if (editor_len < EDITOR_BUF_SIZE - 1) {
             editor_buf[editor_len++] = '\n';
             editor_buf[editor_len] = '\0';
+            editor_cy++; // கர்சரை அடுத்த வரிக்குக் கொண்டு செல்கிறோம்
+            editor_cx = 0;
         }
     }
     
